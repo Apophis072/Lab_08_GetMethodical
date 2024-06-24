9ad20c081b51879a3292dca9631a85eb60a56d1c
Apophis072 <jeffreychen846@gmail.com>
2024-06-24 14:41:14 -0400

    Initial commit

---
+++ src/SafeInput.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class SafeInput
+{
+    /**
+     *
+     * @param pipe a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return a String response that is not zero length
+     */
+    public static String getNonZeroLenString(Scanner pipe, String prompt) {
+        String retString = "";  // Set this to zero length. Loop runs until it isn’t
+        do {
+            System.out.print("\n" + prompt + ": "); // show prompt add space
+            retString = pipe.nextLine();
+        } while (retString.length() == 0);
+
+        return retString;
+
+    }
+
+    public static int getRangedInt(Scanner pipe, String prompt, int low, int high)
+    {
+        int retVal = 0;
+        String trash = "";
+        boolean done = false;
+
+        do
+        {
+            System.out.println("\n" + prompt + "[" + low + "-" + high + "]: ");
+            if (pipe.hasNextInt())
+            {
+                retVal = pipe.nextInt();
+                pipe.nextLine();
+                if (retVal >= low && retVal <= high)
+                {
+                 done = true;
+                }
+                else
+                {
+                    System.out.println("\nNumber is out of the range [" + low + "-" + high + "]: " + retVal);
+                }
+            }
+            else
+            {
+                trash = pipe.nextLine();
+                System.out.println("You must enter an int: " + trash);
+            }
+        } while (!done);
+        return retVal;
+    }
+
+}
